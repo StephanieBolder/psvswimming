59941c366443783b116c314b793f8b93694b4440
bolderstephanie <bolderstephanie@gmail.com>
2020-11-18 15:10:44 +0100

    Merging local and remote repository

--- src/v2/pages/Notifications.vue
+++ src/v2/pages/Notifications.vue
@@ -5,6 +5,9 @@
 </template>
 
 <script>
+import Dropdown from "../../shared/Dropdown.vue";
+import { VueTabs, VTab } from "vue-nav-tabs";
+import "vue-nav-tabs/themes/vue-tabs.css";
 
 export default {
 };
